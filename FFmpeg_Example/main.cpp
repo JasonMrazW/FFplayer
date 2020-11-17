2b718a5c4bbfb71504d3c5b2628008b9b2e63520
bo <wangbo@bilibili.com>
2020-11-17 10:45:34 +0800

    Initial Commit

---
+++ FFmpeg_Example/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FFmpeg_Example
+//
+//  Created by wangbo on 2020/11/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
